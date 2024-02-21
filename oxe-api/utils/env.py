782ed1211d20ce16420d1595a0d0cadb63ae27c0
Shubham Yadav <shubham.yadav@metricoidtech.com>
2024-02-21 18:44:59 +0530

    url fixes

--- oxe-api/utils/env.py
+++ oxe-api/utils/env.py
@@ -4,10 +4,10 @@ from config.config import ENVIRONMENT, BASE_DOMAIN
 def get_admin_portal_url():
     if ENVIRONMENT == "dev":
         return "http://localhost:3000"
-    return "".join(["https://admin.", BASE_DOMAIN])
+    return "".join(["https://openxecoadmin.jenusplanet.com"])
 
 
 def get_community_portal_url():
     if ENVIRONMENT == "dev":
         return "http://localhost:3000"
-    return "".join(["https://", BASE_DOMAIN])
+    return "".join(["https://openxecocommunity.jenusplanet.com"])
